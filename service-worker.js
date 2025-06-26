e83ec9cfb767b1a290adc3efb53d4fbae2b3762a
Pablo Adrados <100553566@alumnos.uc3m.es>
2025-06-26 12:46:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker instalado");
+const CACHE_NAME = 'v1_cache';
+const urlsToCache = [
+  '/muebles-app/',
+  '/muebles-app/index.html',
+  '/muebles-app/manifest.json',
+  '/muebles-app/images/apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-  // No interceptamos nada, solo declaramos el SW
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
